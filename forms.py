93433612144e50aebca47ccf51a90682255d859e
Tim <tfleming26@hotmail.com>
2016-10-19 10:37:26 -0600

    Add sign up users functionality

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class SignupForm(Form):
+    first_name = StringField("First Name", validators=[DataRequired("Please Enter Your First Name.")])
+    last_name = StringField("Last Name", validators=[DataRequired("Please Enter Your Last Name.")])
+    email = StringField("Email", validators=[DataRequired("Please Enter Your Email Address."), Email("Please Enter Your Email Address.")])
+    password = PasswordField("Password", validators=[DataRequired("Please Enter Your Password."), Length(min=6, message="Passwords Must Be 6 Characters or More.")])
+    submit = SubmitField("Sign Up")
